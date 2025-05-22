1efa371136a56d6a0376eeadcf6fe3339cc88c08
4tk666 <vhfxhy5356@yahoo.co.jp>
2025-05-22 10:45:36 +0900

    db、prismaの導入

--- src/auth.ts
+++ src/auth.ts
@@ -1,3 +1,5 @@
+import { prisma } from '@/lib/prisma'
+import { PrismaAdapter } from '@auth/prisma-adapter'
 import NextAuth, { type User, type NextAuthConfig } from 'next-auth'
 import Credentials from 'next-auth/providers/credentials'
 
@@ -5,6 +7,35 @@ import Credentials from 'next-auth/providers/credentials'
 export const BASE_PATH = '/api/auth'
 
 const authOptions: NextAuthConfig = {
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: 'jwt',
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      // ログイン成功
+      // ↓
+      // jwt() コールバック発火
+      // - token に必要な情報を追加
+      // - JWTが生成されCookieに保存
+      if (user) {
+        token.id = user.id
+        token.email = user.email
+        token.name = user.name
+      }
+      return token
+    },
+    async session({ session, token }) {
+      // セッションの更新
+      // セッションにはuser.idが含まれてないので、tokenから取得したidをセッションに追加
+      if (token) {
+        session.user.id = token.id as string
+        session.user.email = token.email as string
+        session.user.name = token.name as string
+      }
+      return session
+    },
+  },
   providers: [
     Credentials({
       name: 'Credentials',
@@ -43,7 +74,7 @@ const authOptions: NextAuthConfig = {
   // シークレットキーの設定
   // セッションの暗号化やJWT署名など、セキュリティ関連の処理に使うシークレットキー
   // NextAuth.js は JWTトークンやセッションデータを暗号化して保管します。
-// そのときにこの secret が使われ、これがないと NextAuthがランダムに生成したキーを毎回使ってしまうため、ログインが持続しない
+  // そのときにこの secret が使われ、これがないと NextAuthがランダムに生成したキーを毎回使ってしまうため、ログインが持続しない
   secret: process.env.NEXTAUTH_SECRET,
 }
 
