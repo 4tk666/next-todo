2f89a4a140409a726f1b94230ceffbb37b122385
4tk666 <vhfxhy5356@yahoo.co.jp>
2025-06-14 21:52:52 +0900

    TODOのUI調整

--- src/components/features/todo/todo-list.tsx
+++ src/components/features/todo/todo-list.tsx
@@ -14,40 +14,40 @@ export default function TodoList({ todosDto }: TodoListProps) {
   const [expandedTodoIds, setExpandedTodoIds] = useState<string[]>([])
 
   return todosDto.length === 0 ? (
-    <tr>
-      <td colSpan={4} className="text-gray-500 text-center py-8">
-        まだTodoがありません。新しいTodoを作成してください。
-      </td>
-    </tr>
+    <div className="text-gray-500 text-center py-8">
+      まだTodoがありません。新しいTodoを作成してください。
+    </div>
   ) : (
-    todosDto.map((todo) => {
-      const isExpanded = expandedTodoIds.includes(todo.id)
-      const hasChildren = todo.children.length > 0
+    <div className="border-t border-gray-200">
+      {todosDto.map((todo) => {
+        const isExpanded = expandedTodoIds.includes(todo.id)
+        const hasChildren = todo.children.length > 0
 
-      return (
-        <Fragment key={todo.id}>
-          <tr className="group border-b border-gray-200">
-            <TodoItem
-              todo={todo}
-              todosDto={todosDto}
-              hasChildren={hasChildren}
-              handleToggleExpansion={(todoId) =>
-                setExpandedTodoIds((previousIds) =>
-                  createExpandedTodoIds({ previousIds, todoId }),
-                )
-              }
-              isExpanded={isExpanded}
-            />
-          </tr>
-          {hasChildren &&
-            isExpanded &&
-            todo.children.map((child) => (
-              <tr key={child.id} className="group border-b border-gray-200">
-                <TodoItem todo={child} todosDto={todosDto} isChildrenTodo />
-              </tr>
-            ))}
-        </Fragment>
-      )
-    })
+        return (
+          <Fragment key={todo.id}>
+            <div className="bg-white">
+              <TodoItem
+                todo={todo}
+                todosDto={todosDto}
+                hasChildren={hasChildren}
+                handleToggleExpansion={(todoId) =>
+                  setExpandedTodoIds((previousIds) =>
+                    createExpandedTodoIds({ previousIds, todoId }),
+                  )
+                }
+                isExpanded={isExpanded}
+              />
+            </div>
+            {hasChildren &&
+              isExpanded &&
+              todo.children.map((child) => (
+                <div key={child.id} className="bg-white ml-6">
+                  <TodoItem todo={child} todosDto={todosDto} isChildrenTodo />
+                </div>
+              ))}
+          </Fragment>
+        )
+      })}
+    </div>
   )
 }
